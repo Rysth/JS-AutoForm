979cb9d8bfacc448a715493195c3f514db3b462b
Rysth <johnpalacios.t@gmail.com>
2023-09-01 13:15:36 -0500

    Add a functionality to move the actual circle placed.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-/* // JavaScript code for drag-and-drop functionality
 function allowDrop(event) {
   event.preventDefault();
 }
-
-function drag(event) {
-  event.dataTransfer.setData('text', event.target.dataset.status);
-}
-
-function drop(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData('text');
-  var circle = document.querySelector(`.status-circle[data-status="${data}"]`);
-  if (circle) {
-    document.querySelector('.auto').appendChild(circle);
-  }
-}
- */
-
-let initialX, initialY;
 
 function dragStart(event) {
   const circle = event.target;
@@ -29,6 +12,7 @@ function dragStart(event) {
 function dragOver(event) {
   event.preventDefault();
 }
+
 function drop(event) {
   event.preventDefault();
   const data = event.dataTransfer.getData('text');
@@ -41,15 +25,77 @@ function drop(event) {
     const x = event.clientX - rect.left - initialX;
     const y = event.clientY - rect.top - initialY;
 
+    // Check if a circle already exists in the desired position
+    const existingCircle = auto.querySelector(
+      `.status-circle.placed[data-status="${data}"]`,
+    );
+
+    if (existingCircle) {
+      auto.removeChild(existingCircle); // Remove the existing circle
+    }
+
     // Clone the original circle
     const newCircle = originalCircle.cloneNode(true);
-    newCircle.classList.add('placed');
+
+    // Calculate the new position within the boundaries of .auto
+    const maxX = rect.width - newCircle.offsetWidth;
+    const maxY = rect.height - newCircle.offsetHeight;
+
+    // Ensure the new position stays within the boundaries
+    const newX = Math.min(Math.max(0, x), maxX);
+    const newY = Math.min(Math.max(0, y), maxY);
 
     // Set the position for the new circle
-    newCircle.style.left = `${x}px`;
-    newCircle.style.top = `${y}px`;
+    newCircle.classList.add('placed');
+    newCircle.style.left = `${newX}px`;
+    newCircle.style.top = `${newY}px`;
+
+    // Make the new circle draggable
+    newCircle.addEventListener('mousedown', startDrag);
 
     // Append the new circle to the container
     auto.appendChild(newCircle);
   }
 }
+
+let initialX, initialY;
+let activeCircle = null;
+let offsetX, offsetY;
+
+function startDrag(event) {
+  activeCircle = event.target;
+  offsetX = event.clientX - activeCircle.getBoundingClientRect().left;
+  offsetY = event.clientY - activeCircle.getBoundingClientRect().top;
+
+  document.addEventListener('mousemove', dragCircle);
+  document.addEventListener('mouseup', stopDrag);
+}
+
+function dragCircle(event) {
+  if (!activeCircle) return;
+
+  const auto = document.querySelector('.auto');
+  const rect = auto.getBoundingClientRect();
+  const x = event.clientX - rect.left - offsetX;
+  const y = event.clientY - rect.top - offsetY;
+
+  const maxX = rect.width - activeCircle.offsetWidth;
+  const maxY = rect.height - activeCircle.offsetHeight;
+
+  const newX = Math.min(Math.max(0, x), maxX);
+  const newY = Math.min(Math.max(0, y), maxY);
+
+  activeCircle.style.left = `${newX}px`;
+  activeCircle.style.top = `${newY}px`;
+}
+
+function stopDrag() {
+  activeCircle = null;
+  document.removeEventListener('mousemove', dragCircle);
+  document.removeEventListener('mouseup', stopDrag);
+}
+
+// Add event listeners to make the circles draggable only once
+document.querySelectorAll('.status-circle[data-status]').forEach((circle) => {
+  circle.addEventListener('mousedown', startDrag);
+});
